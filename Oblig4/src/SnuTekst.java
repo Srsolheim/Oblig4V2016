2d4cd4a67f59564aa8acd098babb388b544ad209
Sofie Solheim <sr.solheim@gmail.com>
2016-04-24 17:43:12 +0200

    2

---
+++ Oblig4/src/SnuTekst.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SnuTekst {
+	static int teller = 0;
+	
+public static void main (String[] args){
+	
+	Scanner scanner = new Scanner(System.in);
+	String input;
+	System.out.println("Skriv inn en string: ");
+	input = scanner.nextLine();
+	System.out.println(baklengs(input));
+}
+
+public static String baklengs(String tekst){
+	if (tekst.length() == 1){
+		System.out.println("Antall tegn: " + teller);
+		return tekst;
+	}
+	teller++;
+	return baklengs(tekst.substring(1)) + tekst.charAt(0);
+}
+}
